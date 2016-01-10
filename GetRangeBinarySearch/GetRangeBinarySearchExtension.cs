cfcf961d9dae3870dba54066e19a4e3dd1023978
Sipos Zoltán <siposz@outlook.com>
2016-01-10 09:53:04 +0100

    First version

---
+++ GetRangeBinarySearch/GetRangeBinarySearchExtension.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetRangeBinarySearch
+{
+    public static class GetRangeBinarySearchExtension
+    {
+        public static List<T> GetRangeBinarySearch<T>(this List<T> source, T from, T to, Comparer<T> comparer = null)
+        {
+            RangeIndex range = GetRangeIndex(source, from, to, comparer);
+            if (range.IsEmpty)
+                return new List<T>();
+
+            return source.GetRange(range.FromIndex, range.ToIndex - range.FromIndex + 1);
+        }
+
+        private static RangeIndex GetRangeIndex<T>(List<T> source, T from, T to, Comparer<T> comparer = null)
+        {
+            //Get the default comparer
+            comparer = comparer ?? Comparer<T>.Default;
+
+            if (source.Count == 0)
+                return RangeIndex.CreateEmpty();
+
+            if (comparer.Compare(from, to) > 0)
+                throw new ArgumentException("from should be smaller or equal than to");
+
+            int fromIndex = source.BinarySearch(from, comparer);
+
+            if (fromIndex < 0)
+                //The first index where the element is larger than from
+                fromIndex = ~fromIndex;
+            else
+                //search for the first matching element
+                while (fromIndex > 0 && comparer.Compare(source[fromIndex - 1], from) == 0)
+                    fromIndex--;
+
+            //All element of the source is smaller then from => return empty
+            if (fromIndex == source.Count)
+                return RangeIndex.CreateEmpty();
+
+            int toIndex = source.BinarySearch(to, comparer);
+
+            if (toIndex < 0)
+            {
+                //This is the last index where the element is smaller than to
+                toIndex = ~toIndex;
+                toIndex--;
+            }
+            else
+                //search for the last matching element
+                while (toIndex < source.Count -1 && comparer.Compare(source[toIndex + 1], to) == 0)
+                    toIndex++;
+
+            //All element of the source is larger then to => return empty
+            if (toIndex < 0)
+                return RangeIndex.CreateEmpty();
+
+            return new RangeIndex(fromIndex, toIndex);
+        }
+
+        private class RangeIndex
+        {
+            public int FromIndex { get; private set; }
+            public int ToIndex { get; private set; }
+
+            public bool IsEmpty { get { return ToIndex < FromIndex; } }
+
+            public static RangeIndex CreateEmpty()
+            {
+                return new RangeIndex(-1, -2);
+            }
+
+            public RangeIndex(int fromIndex, int toIndex)
+            {
+                FromIndex = fromIndex;
+                ToIndex = toIndex;
+            }
+        }
+    }
+}
---
+++ GetRangeBinarySearchTest/GetRangeTest.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using GetRangeBinarySearch;
+using System.Diagnostics;
+using System.Linq;
+
+namespace GetRangeBinarySearchTest
+{
+    [TestClass]
+    public class GetRangeTest
+    {
+        //[TestMethod]
+        //[Ignore]
+        //public void TestMethod1()
+        //{
+        //    int[] ints = new int[3] { 1, 2, 3 };
+
+        //    Assert.AreEqual(1, Array.BinarySearch(ints, 2));
+        //    Assert.AreEqual(2, Array.BinarySearch(ints, 1, ints.Length - 1, 3));
+        //}
+
+        public List<int> intValues = new List<int>() { -5, -2, 0, 0, 1, 2, 3, 4, 4, 4, 4, 6, 6, 8, 9, 11, 11 };
+
+        [TestMethod]
+        public void GetRange_ArgumentException()
+        {
+            try
+            {
+                intValues.GetRangeBinarySearch(-5, -6);
+                Assert.Fail();
+            }
+            catch (ArgumentException)
+            {
+
+            }
+            catch (Exception)
+            {
+                Assert.Fail();
+            }
+
+        }
+
+        [TestMethod]
+        public void GetRange_LargherThanAll()
+        {
+            var range = intValues.GetRangeBinarySearch(12, 12);
+            Assert.IsNotNull(range);
+            Assert.AreEqual(0, range.Count);
+
+            range = intValues.GetRangeBinarySearch(12, 13);
+            Assert.IsNotNull(range);
+            Assert.AreEqual(0, range.Count);
+        }
+
+        [TestMethod]
+        public void GetRange_LowerThanAll()
+        {
+            List<int> range;
+            range = intValues.GetRangeBinarySearch(-6, -6);
+            Assert.IsNotNull(range);
+            Assert.AreEqual(0, range.Count);
+
+            range = intValues.GetRangeBinarySearch(-7, -6);
+            Assert.IsNotNull(range);
+            Assert.AreEqual(0, range.Count);
+        }
+
+        [TestMethod]
+        public void GetRange_EqualFromTo()
+        {
+            List<int> range;
+            range = intValues.GetRangeBinarySearch(-5, -5);
+            Assert.IsNotNull(range);
+            Assert.AreEqual(1, range.Count);
+            Assert.IsTrue(range.SequenceEqual(new List<int> { -5 }));
+
+            range = intValues.GetRangeBinarySearch(-2, -2);
+            Assert.IsNotNull(range);
+            Assert.AreEqual(1, range.Count);
+            Assert.IsTrue(range.SequenceEqual(new List<int> { -2 }));
+
+            range = intValues.GetRangeBinarySearch(0, 0);
+            Assert.IsNotNull(range);
+            Assert.AreEqual(2, range.Count);
+            Assert.IsTrue(range.SequenceEqual(new List<int> { 0, 0 }));
+
+            range = intValues.GetRangeBinarySearch(3, 3);
+            Assert.IsNotNull(range);
+            Assert.AreEqual(1, range.Count);
+            Assert.IsTrue(range.SequenceEqual(new List<int> { 3 }));
+
+            range = intValues.GetRangeBinarySearch(4, 4);
+            Assert.IsNotNull(range);
+            Assert.AreEqual(4, range.Count);
+
+            range = intValues.GetRangeBinarySearch(11, 11);
+            Assert.IsNotNull(range);
+            Assert.AreEqual(2, range.Count);
+        }
+
+        [TestMethod]
+        public void GetRangeSlow()
+        {
+            List<int> range;
+            range = GetRangeSlow(intValues, -6, 3).ToList();
+            Assert.IsNotNull(range);
+            Assert.IsTrue(range.SequenceEqual(new List<int> { -5, -2, 0, 0, 1, 2, 3 }));
+
+
+        }
+
+        [TestMethod]
+        public void GetRange_Full()
+        {
+            List<int> range;
+            range = intValues.GetRangeBinarySearch(-5, 11);
+            Assert.IsNotNull(range);
+            Assert.AreEqual(intValues.Count, range.Count);
+
+            range = intValues.GetRangeBinarySearch(-6, 12);
+            Assert.IsNotNull(range);
+            Assert.AreEqual(intValues.Count, range.Count);
+        }
+
+
+
+        [TestMethod]
+        public void GetRange_MassTest()
+        {
+            int counter = 0;
+            int low = -8;
+            int high = 14;
+            for (int from = low; from <= high; from++)
+                for (int to = from; to <= high; to++)
+                {
+                    GetRangeSlow(intValues, from, to).SequenceEqual(intValues.GetRangeBinarySearch(from, to));
+                    counter++;
+                }
+            int n = high - low + 1;
+            Assert.AreEqual((n * (n + 1)) / 2, counter);
+            Trace.WriteLine(counter);
+        }
+
+        private IEnumerable<T> GetRangeSlow<T>(IEnumerable<T> source, T from, T to, Comparer<T> comparer = null) 
+        {
+            //Get the default comparer
+            comparer = comparer ?? Comparer<T>.Default;
+            return source.Where(e => comparer.Compare(from, e) < 1 && comparer.Compare(to, e) > -1);
+        }
+    }
+}
